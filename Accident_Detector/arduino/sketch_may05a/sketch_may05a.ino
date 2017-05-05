4df7185083f077610a1f8c607efefd3906bb4f62
kshy9598 <kshy9598@naver.com>
2017-05-05 21:28:46 +0900

    arduino 충격센서

---
+++ Accident_Detector/arduino/sketch_may05a/sketch_may05a.ino
@@ -0,0 +1,23 @@
+const int ledPin = 13;      // led connected to digital pin 13
+const int knockSensor = A0; // the piezo is connected to analog pin 0
+const int threshold = 100;  // threshold value to decide when the detected sound is a knock or not
+
+// these variables will change:
+int sensorReading = 0;      // variable to store the value read from the sensor pin
+int ledState = LOW;         // variable used to store the last LED status, to toggle the light
+
+void setup() {
+ pinMode(ledPin, OUTPUT); // declare the ledPin as as OUTPUT
+ Serial.begin(9600);       // use the serial port
+}
+
+void loop() {
+  // read the sensor and store it in the variable sensorReading:
+  sensorReading = analogRead(knockSensor);    
+  
+  ledState = !ledState;     
+  digitalWrite(ledPin, ledState);
+  Serial.println(sensorReading);         
+  
+  delay(100);  // delay to avoid overloading the serial port buffer
+}
